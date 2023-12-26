e67f965c3039dcc3a354629ded8e3fc834492b77
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-12-26 12:18:44 +0200

    uploaded project

---
+++ src/test/java/com/example/First_Lab/FirstLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First_Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
